eb7a84f5c176671ea9865d6f8163caa628f6ff7c
Rishi <rishi2004sh@gmail.com>
2024-12-09 12:22:45 +0530

    add front end files

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      
+    <>
+    <div className='flex justify-between items-center'>
+        <div className='text-black font-bold justify-center left-0 text-xl'>Logo</div>
+        <div className="headerlist"></div>
     </div>
+    </>
   )
 }
 
