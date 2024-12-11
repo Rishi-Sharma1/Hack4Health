21d82fbd0c6af2a3c5c24c845ae64baaabd1f587
Rishi <rishi2004sh@gmail.com>
2024-12-11 22:50:13 +0530

    .

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
       <h1 className="text-4xl font-bold text-black mb-4 bg-white bg-transparent mx-20">
         Analyze Medical Reports Effortlessly
       </h1>
-      <button className="bg-gray-600 text-white px-6 py-2 rounded hover:bg-black-700">
+      <button className="bg-gray-600 text-white px-6 py-2 rounded hover:bg-black-700 hover:bg-orange-700 transition-colors duration-700 rotate-12 hover:rotate-0">
         <NavLink to={'/dashboard'}>
           Submit your first Report
         </NavLink>
@@ -22,26 +22,30 @@ function Home() {
         Leverage AI to extract insights from X-rays, MRIs, CT scans, and more.
       </p>
     </section>
-    <section className="p-12 grid grid-cols-1 md:grid-cols-3 gap-2">
-      <div className="relative p-6 bg-gradient-to-r from-pink-500 to-blue-500 vertical-stripes rounded-3xl">
-      <div className="relative">
-      <h2 className="text-xl font-bold mb-2">X-ray Analysis</h2>
-      <p className="text-gray-900">
-        AI-powered tools to detect fractures and anomalies.
-      </p>
-      </div>
-    </div>
-      <div className="p-6 bg-white shadow-md bg-gradient-to-r from-blue-500 to-orange-500 rounded-3xl">
-        <h2 className="text-xl font-bold mb-2">MRI Insights</h2>
+    <section className="p-12 flex gap-2">
+      <div className="relative p-6 bg-gradient-to-r from-pink-500 to-blue-500 vertical-stripes rounded-2xl hover:rotate-6 ">
+        <div className="duration-500 hover:translate-x-3 hover:text-xl">
+        <h2 className="text-xl font-bold mb-2">X-ray Analysis</h2>
         <p className="text-gray-900">
-          Advanced imaging analysis for neurological and tissue-based data.
+          AI-powered tools to detect fractures and anomalies.
         </p>
+        </div>
       </div>
-      <div className="p-6 bg-gradient-to-r from-orange-500 to-green-500 rounded-3xl">
-        <h2 className="text-xl font-bold mb-2">CT Scan Reports</h2>
-        <p className="text-gray-900 ">
-          Comprehensive interpretation of CT scans using AI.
-        </p>
+      <div className="p-6 bg-white shadow-md bg-gradient-to-r from-blue-500 to-orange-500 rounded-3xl">
+        <div className="duration-500 hover:text-xl">
+          <h2 className="text-xl font-bold mb-2">MRI Insights</h2>
+          <p className="text-gray-900">
+            Advanced imaging analysis for neurological and tissue-based data.
+          </p>
+        </div>
+      </div>
+      <div className="p-6 bg-gradient-to-r from-orange-500 to-green-500 rounded-3xl hover:-rotate-6">
+        <div className="duration-500 hover:text-xl hover:translate--x-3">
+          <h2 className="text-xl font-bold mb-2">CT Scan Reports</h2>
+          <p className="text-gray-900">
+            Comprehensive interpretation of CT scans using AI.
+          </p>
+          </div>
       </div>
     </section>
     </div>
